63ea950fedc5acca1ba901b0498c8e09fbd127cf
turtlehat <89934210+turtle-hat@users.noreply.github.com>
2023-07-04 23:05:48 -0400

    Can download a project now, PHP proxy code added

--- js/main.js
+++ js/main.js
@@ -5,52 +5,103 @@ const DEFAULT_DOWNLOAD_OPTIONS = {
 
 // Page elements
 let inAccountName;
-let inSubmit;
+let btnFetch;
+let btnDownload;
 let outLog
 let outDownloads
 
-// Project data
-let metadata;
-let projects;
-let files;
+// User data retrieved from Scratch API
+let userData
+
+// Project data to be downloaded
+let projectMetadata;
+let projectData;
 
 // Application progress tracking
 let running;
-let xhrJobsRunning;
+let jobsRunning;
 
 window.onload = (e) => {
     // Find elements on page
     inAccountName = document.querySelector("#name");
-    inSubmit = document.querySelector("#submit");
+    btnFetch = document.querySelector("#fetch");
+    btnDownload = document.querySelector("#download");
     outLog = document.querySelector("#log");
     outDownloads = document.querySelector("#downloads");
 
     // Initialize variables
     running = false;
-    xhrJobsRunning = 0;
+    jobsRunning = 0;
 
     // Add functions to site elements
-    inSubmit.onclick = fetch;
-    projects = {};
-    metadata = {};
+    btnFetch.onclick = fetchProjects;
+    btnDownload.onclick = downloadProjects;
+    projectData = {};
+    projectMetadata = {};
 }
 
-function fetch() {
-    if (!running)
+function fetchProjects() {
+    if (!running && inAccountName.value)
     {
         // Prevent from running twice
         running = true;
         // Clear projects, metadata, and log
-        projects = {};
-        metadata = {};
+        projectData = {};
+        projectMetadata = {};
         pageLogClear();
-        // Fetch project, then download info
-        fetchProject();
+        // Fetch project data, then download info
+        fetchUserData(inAccountName.value);
     }
 }
 
-async function fetchProject(id = SAMPLE_PROJECT) {
-    xhrJobsRunning++;
+function downloadProjects() {
+    if (!running && inAccountName.value)
+    {
+        // Prevent from running twice
+        running = true;
+        // Clear projects, metadata, and log
+        projectData = {};
+        projectMetadata = {};
+        pageLogClear();
+        // Fetch project data, then download info
+        fetchProjectData();
+    }
+}
+
+async function fetchUserData(username = "turtlehat") {
+    jobsRunning++
+
+    pageLogUser(`Fetching User Data...`, username);
+
+    let url = `https://api.scratch.mit.edu`;
+    console.log(url);
+
+    // Fetch API code from https://developer.mozilla.org/en-US/docs/Web/API/fetch#examples
+
+    const request = new Request(url, {
+        method: 'GET'
+    });
+
+    console.log(request);
+
+    fetch(request)
+    .then((response) => {
+        console.log(response);
+
+        if (!response.ok) {
+            pageLogUser(`Error occurred fetching user data! ${response.status}`, username);
+            finishJob();
+            return;
+        }
+        
+        console.log(response.text);
+
+        finishJob();
+    });
+}
+
+async function fetchProjectData(id = SAMPLE_PROJECT) {
+    jobsRunning++;
     
     pageLogProject(`Fetching Project...`, id, 1);
 
@@ -77,26 +128,30 @@ async function fetchProject(id = SAMPLE_PROJECT) {
     // First get project metadata
     SBDL.getProjectMetadata(id, downloadOptions)
     .then((m) => {
+        jobsRunning++;
         // If successful, log it in metadata object
         downloadOptions.customOptions.step = 2;
 
         pageLogProject(`Metadata download of ${m.title} successful!`, id, 1);
-        metadata[id] = m;
+        projectMetadata[id] = m;
 
         // Start a new promise to get project data
         SBDL.downloadProjectFromID(id, downloadOptions)
         .then((p) => {
-            pageLogProject(`Download of ${metadata[id].title}.${p.type} successful!`, id, 2);
-            projects[id] = p;
+            pageLogProject(`Download of ${projectMetadata[id].title}.${p.type} successful!`, id, 2);
+            projectData[id] = p;
 
             downloadAll(inAccountName.value);
         })
         .catch((e) => {
             if (e && e.name === 'AbortError') {
-                pageLogProject(`Download of ${metadata[id].title} aborted!`, id, 1);
+                pageLogProject(`Download of ${projectMetadata[id].title} aborted!`, id, 1);
             } else {
-                pageLogProject(`Error occurred downloading ${metadata[id].title}! ${e}`, id, 1);
+                pageLogProject(`Error occurred downloading ${projectMetadata[id].title}! ${e}`, id, 1);
             }
+        })
+        .then(() => {
+            finishJob();
         });
     })
     .catch((e) => {
@@ -107,42 +162,22 @@ async function fetchProject(id = SAMPLE_PROJECT) {
         }
     })
     .then(() => {
-        xhrJobsRunning--;
-        
-        if (xhrJobsRunning <= 0)
-        {
-            xhrJobsRunning = 0;
-            running = false;
-        }
+        finishJob();
     });
 }
 
-// Download file prompt code by Rik Schennink
-// https://pqina.nl/blog/how-to-prompt-the-user-to-download-a-file-instead-of-navigating-to-it/
-
-// function download(file) {
-//     // Create a link to the object as a file
-//     const link = document.createElement("a");
-//     //link.style.display = none;
-//     link.href = URL.createObjectURL(file);
-//     link.download = file.name;
-
-//     // Add link to DOM to allow it to be clicked, then click it
-//     outDownloads.appendChild(link);
-//     link.click();
-// }
 
 // Downloads all project data and metadata in a zip using JSZip
 function downloadAll(label) {
     let zip = new JSZip();
 
     // Zip up the metadata and project file for each project
-    for (let p in projects)
+    for (let p in projectData)
     {
-        zip.file(`${p}-${projects[p].title}.${projects[p].type}`, projects[p].arrayBuffer);
-        zip.file(`${p}-${projects[p].title}.json`, JSON.stringify(metadata[p]));
+        zip.file(`${p}-${projectData[p].title}.${projectData[p].type}`, projectData[p].arrayBuffer);
+        zip.file(`${p}-${projectData[p].title}.json`, JSON.stringify(projectMetadata[p]));
     }
-
+    
     // Save JSZip with filename, include accountName if it exists
     zip.generateAsync({type:"blob"})
     .then(function (blob) {
@@ -150,26 +185,56 @@ function downloadAll(label) {
     });
 }
 
-function pageLog(message) {
-    let logMessage = document.createElement("p");
+function finishJob() {
+    jobsRunning--;
+        
+    if (jobsRunning <= 0)
+    {
+        jobsRunning = 0;
+        running = false;
+    }
+}
+
+function pageLog(message, element = null) {
+    let logMessage = element;
+    if (!element) {
+        logMessage = document.createElement("p")
+    }
     logMessage.innerHTML = message;
     outLog.appendChild(logMessage);
 }
 
-function pageLogProject(message, projectID, step, stepMax = 2) {
-    // Try to find 
-    let logMessage = document.querySelector(`#message-${projectID}-${step}`);
-    // let logMessage = document.createElement("p");
-    if (!logMessage) {
-        logMessage = document.createElement("p");
-        logMessage.id = `message-${projectID}-${step}`
-    }
-    logMessage.innerHTML = `[ ${projectID} | ${step}/${stepMax} ] ${message}`;
-    outLog.appendChild(logMessage);
+function pageLogUser(message, username) {
+    pageLog(
+        `[${username}] ${message}`,
+        pageFindMessage(`message-${username}`)
+    );
 }
 
+function pageLogProject(message, projectID, step, stepMax = 2) {
+    pageLog(
+        `[${projectID}|${step}/${stepMax}] ${message}`,
+        pageFindMessage(`message-${projectID}-${step}`)
+    );
+}
+
+function pageFindMessage(id) {
+    // Try to find existing message with same generated ID
+    let logMessage = document.querySelector(`#${id}`);
+    // If message doesn't exist, make it and set its ID
+    if (!logMessage) {
+        logMessage = document.createElement("p");
+        logMessage.id = id;
+    }
+    return logMessage;
+}
+
+// Code obtained from https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
 function pageLogClear() {
-    outLog.innerHTML = null;
+    while (outLog.firstChild)
+    {
+        outLog.removeChild(outLog.firstChild);
+    }
     // let logMessages = outLog.querySelectorAll("p");
     // console.log(logMessages);
 }
---
+++ php/get-remixes-scratch.php
@@ -0,0 +1,30 @@
+<?php
+	$username = "turtlehat"; // the default project to search for
+    $limit = "40"; // the default amount of projects to display at once
+    $offset = "0"; // the default amount to offset the search
+
+    // if there is ?project, grab the value
+    if(array_key_exists('username', $_GET) && strlen($project)){
+        $project = $_GET['username'];
+        // encode spaces in the parameters as +
+        $project = str_replace(' ', '+', $project);
+    }
+    // if there is ?limit, grab the value
+    if(array_key_exists('limit', $_GET) && strlen($limit)){
+        $limit = $_GET['limit'];
+        // encode spaces in the parameters as +
+        $limit = str_replace(' ', '+', $limit);
+    }
+    // if there is ?offset, grab the value
+    if(array_key_exists('offset', $_GET) && strlen($offset)){
+        $offset = $_GET['offset'];
+        // encode spaces in the parameters as +
+        $offset = str_replace(' ', '+', $offset);
+    }
+
+    // build URL to request from
+	$URL = "https://api.scratch.mit.edu/users/$username/projects?limit=$limit&offset=$offset";
+	header('content-type:application/json');      // tell the requestor that this is JSON
+	header("Access-Control-Allow-Origin: *");     // turn on CORS
+	echo file_get_contents($URL);
+?>
